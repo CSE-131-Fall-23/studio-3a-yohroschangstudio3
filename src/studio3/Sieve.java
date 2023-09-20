6cf2f52b0cd0bd93eecb9c6f7944521be8e8e7a4
zgira <zgira@PC.dhcp.wustl.edu>
2023-09-20 15:36:49 -0500

    studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,40 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("What number do you want to find primes up to?");
+		int max = in.nextInt();
+		
+		boolean[] array = new boolean[max];
+		
+		// Create an array of trues
+		for (int i = 0; i < max; i++)
+			array[i] = true;
+		
+		// Go through 2, 3, 4, 5...
+		for (int i = 2; i < max; i++)
+		{
+			// If the number hasn't been crossed out yet (avoid repetition)
+			if (array[i] == true)
+			{
+				// Go through 2 * 2, 2 * 3, 2 * 4, 2 * 5...
+				for (int j = 2; i * j < max; j++)
+					array[i * j] = false;
+			}	
+		}
+		
+		for (int i = 2; i < max; i++)
+		{
+			// Same as if (array[i] == true)
+			if (array[i]) 
+				System.out.println(i);
+		}
+		
+	}
+
+}
